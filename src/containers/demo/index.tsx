aea5e741061d19ecc3a6af5308ba6dc13f531645
18057950097@163.com <18057950097@163.com>
2021-12-23 14:42:24 +0800

    :sparkles: react-router-dom

---
+++ src/containers/demo/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * Demo
+ */
+import React, { Component } from 'react';
+
+class Demo extends Component {
+  state = {};
+
+  render() {
+    return <div>Demo</div>;
+  }
+}
+
+export default Demo;
---
+++ src/containers/home/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * Home
+ */
+import React, { Component } from 'react';
+
+class Home extends Component {
+  state = {};
+
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+export default Home;
