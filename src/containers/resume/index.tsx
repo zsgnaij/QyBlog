b4e0703b829b248ed12b5b7612dfc12df70adc6d
zsgnaij <18057950097@163.com>
2022-03-15 22:45:01 +0800

    :wrench: 修改图标

--- src/containers/resume/index.tsx
+++ src/containers/resume/index.tsx
@@ -3,6 +3,7 @@
  */
 import React, { Component } from 'react';
 import '@assets/iconfonts/iconfont.css';
+import '@assets/iconfonts/iconfont.js';
 import './index.less';
 
 class Resume extends Component {
@@ -33,22 +34,60 @@ class Resume extends Component {
           </div>
           <div className="qy-row">
             <div className="info-box flex w-full min-w-full sm:w-1/3 sm:min-w-1/3 items-center">
-              <i className="iconfont icon-phone text-2xl" />
+              <svg className="icon text-2xl" aria-hidden="true">
+                <use xlinkHref="#icon-phone" />
+              </svg>
               <code className="h-6 leading-6">18057950097</code>
             </div>
             <div className="info-box flex w-full min-w-full sm:w-1/3 sm:min-w-1/3 items-center">
-              <i className="iconfont icon-wechat text-2xl" />
+              <svg className="icon text-2xl" aria-hidden="true">
+                <use xlinkHref="#icon-wechat" />
+              </svg>
               <code className="h-6 leading-6">jsz952368217</code>
             </div>
             <div className="info-box flex w-full min-w-full sm:w-1/3 sm:min-w-1/3 items-center">
-              <i className="iconfont icon-email text-2xl" />
+              <svg className="icon text-2xl" aria-hidden="true">
+                <use xlinkHref="#icon-mail" />
+              </svg>
               <code className="h-6 leading-6">18057950097@163.com</code>
             </div>
           </div>
-          <h1>教育</h1>
-          <h1>工作经历</h1>
-          <h1>项目经历</h1>
-          <h1>个人</h1>
+          <div className="qy-row">
+            <div className="flex items-center">
+              <svg className="icon text-3xl" aria-hidden="true">
+                <use xlinkHref="#icon-education" />
+              </svg>
+              <h1 className="text-2xl font-medium sm:font-semibold">教育</h1>
+            </div>
+          </div>
+          <div className="qy-row">
+            <div className="flex items-center">
+              <svg className="icon text-3xl" aria-hidden="true">
+                <use xlinkHref="#icon-work" />
+              </svg>
+              <h1 className="text-2xl font-medium sm:font-semibold">
+                工作经历
+              </h1>
+            </div>
+          </div>
+          <div className="qy-row">
+            <div className="flex items-center">
+              <svg className="icon text-3xl" aria-hidden="true">
+                <use xlinkHref="#icon-project" />
+              </svg>
+              <h1 className="text-2xl font-medium sm:font-semibold">
+                项目经历
+              </h1>
+            </div>
+          </div>
+          <div className="qy-row">
+            <div className="flex items-center">
+              <svg className="icon text-3xl" aria-hidden="true">
+                <use xlinkHref="#icon-mine" />
+              </svg>
+              <h1 className="text-2xl font-medium sm:font-semibold">个人</h1>
+            </div>
+          </div>
         </article>
       </div>
     );
